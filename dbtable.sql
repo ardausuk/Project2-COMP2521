995c250e5a1000ebbc08e2d6a0651eb5fc491f90
ardausuk <ausuk388@mtroyal.ca>
2022-11-29 13:56:34 -0700

    adding primary keys

--- dbtable.sql
+++ dbtable.sql
@@ -3,18 +3,21 @@ Creating DB TABLE
 */
 
 CREATE TABLE PIZZA(
+    Pizza_ID NUMERIC ()
 
 )
 
 CREATE TABLE CUSTOMER (
+    Customer_ID NUMERIC ()
 
 )
 
 CREATE TABLE ORDER (
+    Order_ID NUMERIC ()
 
 )
 
 CREATE TABLE INGREDIENT(
-
+    Ingredient_Name NUMERIC ()
 )
 
