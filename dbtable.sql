1001321e2b9edc60285a4085150c4e7d54e63b2f
ardausuk <ausuk388@mtroyal.ca>
2022-12-04 22:15:04 -0700

    dbt pretty much done

--- dbtable.sql
+++ dbtable.sql
@@ -82,9 +82,36 @@ CREATE TABLE PRESET(
 
 
 INSERT INTO PIZZA VALUES
+(223, "12in", 3, "Beef", "Custom"),
+(487, "14in", 2, "Chicken", "Custom"),
+(999, "14in", 1, "Veggies", "Preset");
+
 INSERT INTO CUSTOMER VALUES
+(613 222, "Jack", 403-666-6666, "75 Buffalo Way"),
+(200 002, "Mike", 403-451-5534, "444 Bow Road"),
+(308 556, "Jess", 587-765-1933, "77 Gold Street");
+
 INSERT INTO ORDER VALUES
+(1, 613 222, 4, "Good pizza", "04/15/2022", "12:30", "Picked up", "23", "Pick up"),
+(2, 200 002, 3, "Good toppings", "03/21/2022", "14:47", "Picked up", "19.50", "Pick up"),
+(3, 308 556, 5, "Very tasty", "02/25/2022", "17:51", "Ready for pick up", "25", "Delivery");
+
 INSERT INTO INGREDIENT VALUES
+("Beef", "Meat", "Extra"),
+("Ranch", "Sauce", "Extra"),
+("Onions", "Veggies", "light");
+
 INSERT INTO DELIVERY VALUES
+(1, 612 222, 111, "12km"),
+(2, 200 002, 222, "20km"),
+(3, 308 556, 333, "35km");
+
 INSERT INTO CUSTOM VALUES
+(223, "Broccoli"),
+(487, "Chicken"),
+(999, "Olives");
+
 INSERT INTO PRESET VALUES
+(223, "Butter Chicken", "Chicken, Cheese, Curry"),
+(487, "Buffalo Chicken", "Chicken, Hot sauce, Onions"),
+(999, "Cheese", "Gouda, Mozzarella, Feta");
